54af24c34d31c08b71cbcfc271753343e5f19494
ittirarichan <abhishekbinish86@gmail.com>
2024-11-29 11:29:33 +0530

    api

---
+++ rest_api/app/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import *
+
+class Sample(serializers.Serializer):
+    name=serializers.CharField()
+    age=serializers.IntegerField()
+    email=serializers.EmailField()
+    place=serializers.CharField()
+
+
+
+class model_serializer(serializers.ModelSerializer):
+    class Meta:
+        model=Rest_user
+        fields='__all__'
